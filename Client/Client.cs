65c486733714d224286151cefb346dc1ab3515c2
SimonThor12 <simon.lars.thor@gmail.com>
2025-10-20 14:05:40 +0200

    Renaming the scaffolded project

--- Client/Client.cs
+++ Client/Client.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Client
 {
-    public class Game1 : Game
+    public class Client : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public Client()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new Client.Game1();
+﻿using var game = new Client.Client();
 game.Run();
